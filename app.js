961e399c4ca414b94ffed843990766f5782c7981
Travis Alstrand <travisalstrand@gmail.com>
2022-12-20 13:52:08 -0800

    home page finished

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const { projects } = require('./data.json');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// SET VIEW ENGINE
+app.set('view engine', 'pug');
 
+// STATIC MIDDLEWARE
+app.use('/static', express.static('public'));
+
+// HOME ROUTE
+app.get('/', (req, res) => {
+  res.render('index', { projects });
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
