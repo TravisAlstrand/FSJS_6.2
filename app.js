913b0b46ba3155e365394c7bff15f6f948a7cfbd
Travis Alstrand <travisalstrand@gmail.com>
2022-12-20 13:59:08 -0800

    about page finished

--- app.js
+++ app.js
@@ -13,5 +13,10 @@ app.get('/', (req, res) => {
   res.render('index', { projects });
 });
 
+// ABOUT ROUTE
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
