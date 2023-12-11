ea7b448659f830d538e4a9e6565da450e7ef7307
Travis Alstrand <travisalstrand@gmail.com>
2023-12-11 14:25:17 -0800

    test 1

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const { projects } = require('./data.json');
+const express = require("express");
+const { projects } = require("./data.json");
 const app = express();
 
 // SET VIEW ENGINE
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 // STATIC MIDDLEWARE
-app.use('/static', express.static('public'));
+app.use("/static", express.static("public"));
 
 // HOME ROUTE
-app.get('/', (req, res, next) => {
-  res.render('index', { projects });
+app.get("/", (req, res, next) => {
+  res.render("index", { projects });
 });
 
 // ABOUT ROUTE
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 // PROJECT ROUTE
-app.get('/project/:id', (req, res, next) => {
+app.get("/project/:id", (req, res, next) => {
   const project = projects[req.params.id];
   if (project) {
-    res.render('project', { project });
+    res.render("project", { project });
   } else {
     next();
-  };
+  }
 });
 
 // 404 HANDLER
@@ -40,14 +40,17 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   if (err.status === 404) {
     console.log(`${err.message} Status: ${err.status}`);
-    res.render('page-not-found', { err });
+    res.render("page-not-found", { err });
   } else if (err.status === 500) {
     err.status = 500;
-    err.message = 'Sorry, there seems to be a server issue!';
-    res.render('error', { err });
-    console.log(`${err.message} Status: ${err.status}`)
+    err.message = "Sorry, there seems to be a server issue!";
+    res.render("error", { err });
+    console.log(`${err.message} Status: ${err.status}`);
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT);
+//   , () =>
+//   // console.log(`Server running on port ${PORT}`)
+// );
