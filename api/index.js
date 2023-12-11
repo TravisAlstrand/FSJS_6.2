89cb3382b1c3289a6a4b38f07768487fc3bc8662
Travis Alstrand <travisalstrand@gmail.com>
2023-12-11 14:42:50 -0800

    changed index.js

--- api/index.js
+++ api/index.js
@@ -13,4 +13,58 @@ app.get("/api/item/:slug", (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
+const { projects } = require("./data.json");
+
+// SET VIEW ENGINE
+app.set("view engine", "pug");
+
+// STATIC MIDDLEWARE
+app.use("/static", express.static("public"));
+
+// HOME ROUTE
+app.get("/", (req, res, next) => {
+  res.render("index", { projects });
+});
+
+// ABOUT ROUTE
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// PROJECT ROUTE
+app.get("/project/:id", (req, res, next) => {
+  const project = projects[req.params.id];
+  if (project) {
+    res.render("project", { project });
+  } else {
+    next();
+  }
+});
+
+// 404 HANDLER
+app.use((req, res, next) => {
+  const err = new Error();
+  err.status = 404;
+  err.message = "That page doesn't exist!";
+  next(err);
+});
+
+// GLOBAL ERROR HANDLER
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    console.log(`${err.message} Status: ${err.status}`);
+    res.render("page-not-found", { err });
+  } else if (err.status === 500) {
+    err.status = 500;
+    err.message = "Sorry, there seems to be a server issue!";
+    res.render("error", { err });
+    console.log(`${err.message} Status: ${err.status}`);
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
 module.exports = app;
