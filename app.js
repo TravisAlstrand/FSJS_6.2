801c92f82f8e73ac9b6a0356c6a7fc3d75d66336
Travis Alstrand <travisalstrand@gmail.com>
2023-12-11 15:09:37 -0800

    removed rewrite line vercel.json

--- app.js
+++ app.js
@@ -50,6 +50,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
